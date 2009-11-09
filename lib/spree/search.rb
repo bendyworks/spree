d95504feb379362fa11f4b327fcfddcaaf230d57
Roman Smirnov <roman@railsdog.com>
2009-11-10 04:57:17 +0800

    Added properties hash to searcher object and method to prepare search using params data.
    [#882 state:resolved]

--- lib/spree/search.rb
+++ lib/spree/search.rb
@@ -1,5 +1,6 @@
 module Spree::Search
   def retrieve_products
+    Spree::Config.searcher.prepare(params)
     # taxon might be already set if this method is called from TaxonsController#show
     @taxon ||= Taxon.find_by_id(params[:taxon]) unless params[:taxon].blank?
     # add taxon id to params for searcher
