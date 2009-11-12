722d35ca69b4cb8ad0df991f5d84ee67017f20ce
Roman Smirnov <roman@railsdog.com>
2009-11-13 04:38:23 +0800

    Autocomplete with Rails helper replaced by jQuery.AutoComplete
    [#859 state:resolved]

--- app/metal/seo_assist.rb
+++ app/metal/seo_assist.rb
@@ -6,7 +6,6 @@ class SeoAssist
   
   def self.call(env)
     request = Rack::Request.new(env)
-<<<<<<< HEAD
     params = request.params
     taxon_id = params['taxon']
     if !taxon_id.blank? && !taxon_id.is_a?(Hash) && @taxon = Taxon.find(taxon_id)
@@ -28,15 +27,4 @@ class SeoAssist
       end
     }.join("&")
   end
-=======
-    params = request.params             
-    if !params['taxon'].blank? && @taxon = Taxon.find(params['taxon'])
-      params.delete('taxon')
-      get_query = params.map{|k,v| "#{k}=#{v}"}.join('&')
-      return [301, {'Location'=> "/t/#{@taxon.permalink}?#{get_query}" }, []]
-    end
-    [404, {"Content-Type" => "text/html"}, "Not Found"]
-  end
->>>>>>> Created first version of SeoAssist; fixed related problem with serach by taxon.
-    
 end
